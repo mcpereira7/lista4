cc1ae9963cab57268a85015a65d4fef9bcfa4353
marcelo.cpereira4 <marcelo.cpereira4@CAS555572W0719.senacsp.edu.br>
2017-06-12 21:31:29 -0300

    Lista 4 - Exec 1

---
+++ lista4/src/lista4/Lista4.java
@@ -0,0 +1,15 @@
+package lista4;
+
+import static lista4.exec1.vetorInt;
+
+public class Lista4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        vetorInt();
+    }
+    
+}
